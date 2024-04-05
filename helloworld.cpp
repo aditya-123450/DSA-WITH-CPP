652ebcd7505f3a2cfdd780fb2886db7dc9f296d1
aditya-123450 <shivam980101@gmail.com>
2024-04-05 15:06:10 +0530

    hellow world

---
+++ helloworld.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+
+//standared namespace declaration
+using namespace std;
+
+//main function
+int main()
+{
+    //standared output statement
+    cout<<"My first c++ program";
+
+    //wait for output screen
+    getch();
+
+    //main function return statement
+    return 0;
+}
