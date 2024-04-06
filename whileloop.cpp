2bcc7051dc09bd7bd6b57ccb7105f817c4229c76
aditya-123450 <shivam980101@gmail.com>
2024-04-06 15:29:54 +0530

    while loop program

---
+++ whileloop.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    //variable declaration 
+    int a;
+
+    //get input value
+    cout<<"Enter the Number: ";
+    cin>>a;
+
+    int counter=1;
+
+    //while loop block
+    while(counter<=a)
+    {
+        cout<<"Execute While "<<counter<<" time"<<endl;
+        counter++;
+    }
+
+    //wait for output screeen
+    
+    return 0;
+}
