abecbd9c4a93b5ba0626fd4b3bdf49858c089b62
aditya-123450 <shivam980101@gmail.com>
2024-04-06 16:05:35 +0530

    constructor program

---
+++ constructor.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+class example{
+    //variable decleration
+    int a,b;
+    public:
+
+    //constructor
+    example()  {
+
+    //assign values in constructor
+    a=15;
+    b=20;
+
+    cout<<"Im constructor\n";
+    }
+
+void Display(){
+    cout<<"values: "<<a<<"\t"<<b;
+}
+};
+int main(){
+    example object;
+
+    //constructor invoked
+    object.Display();
+
+    return 0;
+
+}
+
+
+
+
+
