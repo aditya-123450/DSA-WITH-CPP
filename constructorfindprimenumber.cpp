762508e35c81f56d1d17b6421d0cb2e83b2576d3
aditya-123450 <shivam980101@gmail.com>
2024-04-07 22:09:22 +0530

    constructor program find prime number

---
+++ constructorfindprimenumber.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+//class Declaration
+class prime 
+{
+    //member variable decleration
+    int a,k,i;
+    public:
+
+    prime(int x)
+    {
+        a=x;
+
+        k=1;
+        {
+            for(i=2; i<=a/2; i++)
+            if(a%i==0)
+            {
+                k=0;
+                break;
+            }
+            else
+            {
+                k=1;
+            }
+        }
+    }
+    void show()
+    {
+        if(k==1)
+        cout<<"\n"<<a<<" is Prime Number: ";
+        else
+        cout<<"\n"<<a<<" is Prime Number: ";
+    }
+};
+
+//main function
+int main()
+{
+    int a;
+    cout<<"Enter the Number: ";
+    cin>>a;
+
+    //object creation for class
+    prime obj(a);
+
+    //call member function
+    obj.show();
+
+    return 0;
+}
