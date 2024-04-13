99713ae7b557dd0593607d676377a18245469a5b
aditya-123450 <shivam980101@gmail.com>
2024-04-13 22:26:14 +0530

    program for exception handiling with multiple catch

---
+++ ExceptionHandingwithmultiplecatch.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+void test(int x)
+{
+    try
+    {
+        if(x>0)
+        throw x;
+        else 
+            throw 'x';
+    }
+    catch(int x)
+    {
+        std::cout<<" \nCatch a integer and that intrger is : \n"<<x;
+    }
+    catch(char x)
+    {
+        std::cout<<"\n catch a character and that character is : \n "<<x;
+    }
+}
+int main()
+{
+    std::cout<<"Testing multiple catches \n: ";
+    test(10);
+    test(0);
+}
