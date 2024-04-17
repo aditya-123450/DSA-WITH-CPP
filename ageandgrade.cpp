0fbd81c931bd98653349e1fee470dacb93b5aad8
aditya-123450 <shivam980101@gmail.com>
2024-04-18 00:24:18 +0530

    enter your age and print if you should bi in grade 10

---
+++ ageandgrade.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int age;
+cout << "Enter your present age : " << endl;
+cin>>age;
+if(age==16)
+{
+cout << "Your present age is " << age << " years." << endl;
+cout << "You are of the right age for joining grade 10 !" << endl;
+}
+else
+{
+cout << "Your present age is " << age << " years." << endl;
+cout << "You are not of the right age for joining grade 10 !" << endl;
+}
+}
