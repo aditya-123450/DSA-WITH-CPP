beaf228b6f85b6373f1ac0e5d747e702d2ab88b7
aditya-123450 <shivam980101@gmail.com>
2024-04-17 23:47:45 +0530

    output an integer a floating point number and a character

---
+++ outputinteger.cpp
@@ -0,0 +1,15 @@
+/*Program to output an integer, a
+floating point number and a character*/
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int x = 10;
+float y = 10.1;
+char z = 'a';
+cout << "x = " << x << endl;
+cout << "y = " << y << endl;
+cout << "z = " << z << endl;
+
+}
