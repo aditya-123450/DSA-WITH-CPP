82e864fa6f34bb524fae546091963a3dfb67f5ff
aditya-123450 <shivam980101@gmail.com>
2024-04-18 00:04:47 +0530

    program for find the sum difference product quotient of two integers

---
+++ findsumdifferenceproductandquotientoftwointegers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int x = 10;
+int y = 2;
+int sum, difference, product, quotient;
+sum = x + y;
+difference = x - y;
+product = x * y;
+quotient = x / y;
+cout << "The sum of " << x << " & " << y << " is " << sum << "." << endl;
+cout << "The difference of " << x << " & " << "y << is " << difference << "." << endl;
+cout << "The product of " << x << " & " << y << " is " << product << "." << endl;
+cout << "The quotient of " << x << " & " << y << " is " << quotient << "." << endl;
+
+}
