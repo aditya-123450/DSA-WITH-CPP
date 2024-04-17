e8bb8ba677c201a0a2fe56cc401686e36ca73d07
aditya-123450 <shivam980101@gmail.com>
2024-04-17 23:34:07 +0530

    adds two integers

---
+++ addstwointegers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main() 
+{
+// A simple C++ program
+int x, y, sum;
+cout << "A program which adds two integers\n";
+cout << "Enter 1st integer: ";
+cin >> x;
+cout << "Enter 2nd integer: ";
+cin >> y;
+sum = x + y;
+cout << "Sum is " << sum << endl;
+return 0;
+}
