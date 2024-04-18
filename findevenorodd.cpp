9b0c619a7e6cdad6e32541ec6aabf6b08093a9fe
aditya-123450 <shivam980101@gmail.com>
2024-04-18 12:09:09 +0530

    even or odd program

---
+++ findevenorodd.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int x;
+cout << "Enter an integer : ";
+cin>>x;
+if(x%2==0)
+cout << "The number " << x << " is even.";
+else
+cout << "The number " << x << " is odd.";
+}
