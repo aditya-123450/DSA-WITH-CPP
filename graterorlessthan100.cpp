ec19baef84123b3c042573c19fa5c9cce2c78a50
aditya-123450 <shivam980101@gmail.com>
2024-04-18 11:52:30 +0530

    grater or less than 100 program

---
+++ graterorlessthan100.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int x;
+cout << "Enter an integer : " << endl;
+cin>>x;
+if(x>100)
+{
+cout << x << " is greater than 100." << endl;
+}
+else
+{
+cout << x << " is less than 100." << endl;
+}
+}
