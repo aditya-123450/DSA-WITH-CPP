da025f6b1e26aa92ad85fac9635938167ea85179
aditya-123450 <shivam980101@gmail.com>
2024-04-22 19:48:46 +0530

    program to enter a character and output its ASCII code

---
+++ ASCIIcode.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+char charac;
+cout << "Enter the character : " << endl;
+cin>>charac;
+int num1=charac;
+cout << "The ASCII code for " << charac << " is " << num1 << "." << endl;
+}
