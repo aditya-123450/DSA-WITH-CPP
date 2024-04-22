6380de6adbe2601efc3a442c0dbc178ad2f7de70
aditya-123450 <shivam980101@gmail.com>
2024-04-22 20:02:11 +0530

    working of getline program

---
+++ getline.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+string str;
+int t = 4;
+while (t--)
+{
+// Read a line from standard input in str
+getline(cin, str);
+cout << str << " : newline" << endl;
+}
+return 0;
+}
+
