29efc01124595cdc630ad353f2c893eb62431e70
aditya-123450 <shivam980101@gmail.com>
2024-04-27 15:51:17 +0530

    pointer variable program

---
+++ pointervariable.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class Point {
+private:
+int x;
+int y;
+public:
+Point(int i, int j); // Constructor
+};
+Point::Point(int i = 0, int j = 0) {
+x = i;
+y = j;
+cout << "Constructor called";
+}
+int main()
+{
+Point t1, *t2;
+return 0;
+}
